91eebc93d4f1834f983b86d1e214bb5f6ca8e6e5
费久猛(80004960) <hubeixuegao@gmail.com>
2020-08-16 15:40:48 +0800

    1

---
+++ src/test/java/com/xuegao/xuegao_first_spring_boot_starter/XuegaoFirstSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.xuegao_first_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuegaoFirstSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
